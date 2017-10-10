7d17b161287c24bc8eb66844f8b81d584e2e1704
Tristan Magniez <Doc0160@users.noreply.github.com>
2017-10-11 01:08:17 +0200

    Update evil.ps1

--- evil.ps1
+++ evil.ps1
@@ -1,9 +1,7 @@
 #Set-ExecutionPolicy -ExecutionPolicy Bypass
 
-#(New-Object System.Net.WebClient).DownloadFile('http://random.cat/i/1249647860.jpg', 'cat.jpg')
-#Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "$($pwd)\cat.jpg"
-#rundll32.exe user32.dll, UpdatePerUserSystemParameters
-#rundll32.exe user32.dll, UpdatePerUserSystemParameters
-#rundll32.exe user32.dll, UpdatePerUserSystemParameters
-
-calc
+(New-Object System.Net.WebClient).DownloadFile('http://random.cat/i/1249647860.jpg', 'cat.jpg')
+Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "$($pwd)\cat.jpg"
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+rundll32.exe user32.dll, UpdatePerUserSystemParameters

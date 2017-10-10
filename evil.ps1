bd4719913b5dbf0a3902bd1bc84d834f303e79c5
Tristan Magniez <Doc0160@users.noreply.github.com>
2017-10-11 01:12:11 +0200

    Update evil.ps1

--- evil.ps1
+++ evil.ps1
@@ -3,5 +3,7 @@
 (New-Object System.Net.WebClient).DownloadFile('http://random.cat/i/1249647860.jpg', 'cat.jpg')
 Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "$($pwd)\cat.jpg"
 rundll32.exe user32.dll, UpdatePerUserSystemParameters
+Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "$($pwd)\cat.jpg"
 rundll32.exe user32.dll, UpdatePerUserSystemParameters
+Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "$($pwd)\cat.jpg"
 rundll32.exe user32.dll, UpdatePerUserSystemParameters

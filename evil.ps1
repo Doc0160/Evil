2458228d2481c1528104cf5c557cbf3676c591ae
Tristan Magniez <Doc0160@users.noreply.github.com>
2017-10-11 00:25:52 +0200

    Update evil.ps1

--- evil.ps1
+++ evil.ps1
@@ -1,4 +1,4 @@
-Set-ExecutionPolicy -ExecutionPolicy Bypass
+
 
 (New-Object System.Net.WebClient).DownloadFile('http://random.cat/i/1249647860.jpg', 'cat.jpg')
 Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "$($pwd)\cat.jpg"
